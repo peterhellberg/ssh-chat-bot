c3858924952155c211ef3da344bcce2e8f9d3174
Peter Hellberg <peter@c7.se>
2015-01-10 22:09:40 +0100

    Echo bot

---
+++ robots/echo.go
@@ -0,0 +1,20 @@
+package robots
+
+import "strings"
+
+// EchoBot is a simple ping/pong bot
+type EchoBot struct{}
+
+func init() {
+	RegisterRobot("echo", func() (robot Robot) { return new(EchoBot) })
+}
+
+// Run executes a command
+func (b EchoBot) Run(c *Command) string {
+	return strings.Join(c.Args, " ")
+}
+
+// Description describes what the robot does
+func (b EchoBot) Description() string {
+	return "what you said"
+}
