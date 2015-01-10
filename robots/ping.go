4a9c60a6ff8504af50e95c5d64bcca0fbe16dabe
Peter Hellberg <peter@c7.se>
2015-01-10 22:09:50 +0100

    Ping bot

---
+++ robots/ping.go
@@ -0,0 +1,18 @@
+package robots
+
+// PingBot is a simple ping/pong bot
+type PingBot struct{}
+
+func init() {
+	RegisterRobot("ping", func() (robot Robot) { return new(PingBot) })
+}
+
+// Run executes a command
+func (b PingBot) Run(c *Command) string {
+	return "pong"
+}
+
+// Description describes what the robot does
+func (b PingBot) Description() string {
+	return "pong"
+}
