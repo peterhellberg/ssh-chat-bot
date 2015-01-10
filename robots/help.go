620af6ca656b625677a361188b827a004403addf
Peter Hellberg <peter@c7.se>
2015-01-10 23:30:56 +0100

    New help output format

--- robots/help.go
+++ robots/help.go
@@ -1,5 +1,7 @@
 package robots
 
+import "strings"
+
 // HelpBot is a robot that list all the robots
 type HelpBot struct{}
 
@@ -9,18 +11,24 @@ func init() {
 
 // Run returns a list of all robots
 func (b HelpBot) Run(c *Command) string {
-	output := ""
+	outputs := []string{}
 
 	for name, fn := range Robots {
 		if name != "help" {
-			output = output + name + "{" + fn().Description() + "} "
+			output := name
+
+			if desc := fn().Description(); desc != "" {
+				output += " " + desc
+			}
+
+			outputs = append(outputs, output)
 		}
 	}
 
-	return output
+	return strings.Join(outputs, ", ")
 }
 
 // Description describes what the robot does
 func (b HelpBot) Description() string {
-	return "List commands!"
+	return ""
 }
