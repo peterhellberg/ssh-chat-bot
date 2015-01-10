94b2b4f9d1e2824bdbfb5c3bfd4f481799eb3245
Peter Hellberg <peter@c7.se>
2015-01-10 22:43:28 +0100

    Flip bot

---
+++ robots/flip.go
@@ -0,0 +1,33 @@
+package robots
+
+import (
+	"strings"
+
+	"github.com/peterhellberg/flip"
+)
+
+// FlipBot is a bot who flips text
+type FlipBot struct{}
+
+func init() {
+	RegisterRobot("flip", func() (robot Robot) { return new(FlipBot) })
+}
+
+// Run executes a deferred action
+func (b FlipBot) Run(c *Command) string {
+	switch c.Args[0] {
+	default:
+		return flip.UpsideDown(strings.Join(c.Args, " "))
+	case "table", "t":
+		return flip.Table(strings.Join(c.Args[1:], " "))
+	case "gopher", "g":
+		return flip.Gopher(strings.Join(c.Args[1:], " "))
+	}
+
+	return ""
+}
+
+// Description describes what the robot does
+func (b FlipBot) Description() string {
+	return "the table"
+}
