919a6e1a6fb929c0d7851c06b21dee06c9b1885c
Peter Hellberg <peter@c7.se>
2018-01-04 00:43:22 +0100

    Replace otto with goja
    https://github.com/dop251/goja

--- robots/eval.go
+++ robots/eval.go
@@ -1,16 +1,12 @@
 package robots
 
 import (
-	"errors"
 	"fmt"
-	"os"
 	"strings"
 	"time"
 
-	"github.com/robertkrimen/otto"
+	"github.com/dop251/goja"
 )
-
-var errHalt = errors.New("Halt!")
 
 // EvalBot is a bot that evaluates JavaScript
 type EvalBot struct{}
@@ -21,11 +17,15 @@ func init() {
 
 // Run executes a command
 func (b EvalBot) Run(c *Command) string {
-	js := strings.Join(c.Args, " ")
+	vm := goja.New()
 
-	value, err := b.runUnsafe(js)
+	time.AfterFunc(1*time.Second, func() {
+		vm.Interrupt("JS timeout")
+	})
+
+	value, err := vm.RunString(strings.Join(c.Args, " "))
 	if err != nil {
-		return ""
+		return err.Error()
 	}
 
 	out := fmt.Sprintf("%v", value)
@@ -50,32 +50,4 @@ func (b EvalBot) Run(c *Command) string {
 // Description describes what the robot does
 func (b EvalBot) Description() string {
 	return "<js>"
-}
-
-func (b EvalBot) runUnsafe(unsafe string) (otto.Value, error) {
-	start := time.Now()
-	defer func() {
-		duration := time.Since(start)
-		if caught := recover(); caught != nil {
-			if caught == errHalt {
-				fmt.Fprintf(os.Stderr, "Some code took to long! Stopping after: %v\n", duration)
-				return
-			}
-
-			return
-		}
-		fmt.Fprintf(os.Stderr, "Ran code successfully: %v\n", duration)
-	}()
-
-	vm := otto.New()
-	vm.Interrupt = make(chan func(), 1) // The buffer prevents blocking
-
-	go func() {
-		time.Sleep(2 * time.Second) // Stop after two seconds
-		vm.Interrupt <- func() {
-			panic(errHalt)
-		}
-	}()
-
-	return vm.Run(unsafe) // Here be dragons (risky code)
 }
